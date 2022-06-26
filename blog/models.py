3c1e7397bc47f112886c64ac6f3803eaf1c19a83
alfa23 <paw235@gmail.com>
2022-06-26 16:48:06 +0000

    add: Main views code & wire up

--- blog/models.py
+++ blog/models.py
@@ -8,14 +8,16 @@ class Post(models.Model):
 
     title = models.CharField(max_length=200, unique=True)
     slug = models.SlugField(max_length=200, unique=True)
-    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="blog_posts")
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="blog_posts")
     updated_on = models.DateTimeField(auto_now=True)
     content = models.TextField()
     featured_image = CloudinaryField('image', default='placeholder')
     excerpt = models.TextField(blank=True)
     created_on = models.DateTimeField(auto_now_add=True)
     status = models.IntegerField(choices=STATUS, default=0)
-    likes = models.ManyToManyField(User, related_name='blog_likes', blank=True)
+    likes = models.ManyToManyField(
+        User, related_name='blogpost_like', blank=True)
 
     class Meta:
         ordering = ['-created_on']
@@ -24,7 +26,7 @@ class Post(models.Model):
         return self.title
 
     def number_of_likes(self):
-        return self.like.count()
+        return self.likes.count()
 
 class Comment(models.Model):
 
